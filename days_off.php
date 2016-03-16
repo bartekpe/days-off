187aa00522a73ce48601ed780cc7eed8ee3fd9f2
Bartek Paul <bartlomiej.paul@gmail.com>
2016-03-16 08:24:11 +0100

    remove unnecessary date

--- days_off.php
+++ days_off.php
@@ -4,8 +4,7 @@
         date('Y-01-06'), // Trzech Króli 
         date('Y-m-d',strtotime('+1 day',easter_date())), // Wielkanoc 2 dzień 
         date('Y-05-01'), // 1 maja - Święto Państwowe     
-        date('Y-05-03'), // 3 maja =  Święto Narodowe Trzeciego Maja
-        date('Y-m-d',strtotime('+49 days',easter_date())), //  Zesłanie Ducha Świętego 
+        date('Y-05-03'), // 3 maja =  Święto Narodowe Trzeciego Maja        
         date('Y-m-d',strtotime('+60 days',easter_date())), //  Boże Ciało     
         date('Y-08-15'), // Wniebowzięcie Najświętszej Marii Panny / Święto Wojska Polskiego    
         date('Y-11-01'), // Uroczystość Wszystkich Świętych    
