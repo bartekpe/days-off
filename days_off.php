d72c2c84c020f28ad65fb4fd20c7eb066f2ecc21
Bartek Paul <bartlomiej.paul@gmail.com>
2016-03-16 08:18:58 +0100

    wrong calculation in date range

--- days_off.php
+++ days_off.php
@@ -5,7 +5,7 @@
         date('Y-m-d',strtotime('+1 day',easter_date())), // Wielkanoc 2 dzień 
         date('Y-05-01'), // 1 maja - Święto Państwowe     
         date('Y-05-03'), // 3 maja =  Święto Narodowe Trzeciego Maja
-        date('Y-m-d',strtotime('+50 days',easter_date())), //  Zesłanie Ducha Świętego 
+        date('Y-m-d',strtotime('+49 days',easter_date())), //  Zesłanie Ducha Świętego 
         date('Y-m-d',strtotime('+60 days',easter_date())), //  Boże Ciało     
         date('Y-08-15'), // Wniebowzięcie Najświętszej Marii Panny / Święto Wojska Polskiego    
         date('Y-11-01'), // Uroczystość Wszystkich Świętych    
